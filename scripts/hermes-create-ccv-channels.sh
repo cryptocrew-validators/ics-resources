515c9c82d9ea24151fc38e265bccc30dd974a1a3
clemensgg <csc@soundportal.at>
2023-09-28 14:48:22 +0200

    add relayer script

--- scripts/hermes-create-ccv-channels.sh
+++ scripts/hermes-create-ccv-channels.sh
@@ -8,15 +8,6 @@ CONSUMER_CHAIN_ID="<YOUR-CONSUMERCHAIN-ID>"
 PROVIDER_CLIENT_ID="<YOUR-PROVIDERCHAIN-IBC-CLIENT-ID>"
 PROVIDER_CHAIN_ID="<YOUR-PROVIDERCHAIN-ID>"
 
-# CONSUMER_CLIENT_ID is on CONSUMER upon genesis
-CONSUMER_CLIENT_ID="07-tendermint-126"
-CONSUMER_CHAIN_ID="pion-1"
-
-# PROVIDER_CLIENT_ID is created on PROVER upon CONSUMER spawn time: gaiad q provider list-consumer-chains
-PROVIDER_CLIENT_ID="07-tendermint-48"
-PROVIDER_CHAIN_ID="provider"
-
-
 CONFIG=$1
 if [ -z "$CONFIG" ]; then 
     CONFIG=$HOME/.hermes/config.toml
